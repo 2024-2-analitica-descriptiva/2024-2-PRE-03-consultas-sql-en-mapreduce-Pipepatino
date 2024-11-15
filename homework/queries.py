0fa56ec7ce3983a0ebfbb82400f559e302e613f4
Pipepatino <felipequimic99@gmail.com>
2024-11-15 15:58:11 -0500

    Creacion de archivos y funciones iniciales

--- homework/queries.py
+++ homework/queries.py
@@ -1,7 +1,26 @@
-"""Taller evaluable"""
 
+"""Taller evaluable"""
 # pylint: disable=broad-exception-raised
 # pylint: disable=import-error
+
+from .mapreduce import run_mapreduce_job # type: ignore
+
+#
+# Columns:
+# total_bill, tip, sex, smoker, day, time, size
+#
+
+#
+# SELECT *, tip/total_bill as tip_rate
+# FROM tips;
+#
+def mapper_query_1(sequence):
+    print(sequence)
+
+
+def reducer_query_1(sequence):
+    """Reducer"""
+    return sequence
 
 
 #
@@ -9,6 +28,13 @@
 #
 def run():
     """Orquestador"""
+
+    run_mapreduce_job(
+        mapper=mapper_query_1,
+        reducer=reducer_query_1,
+        input_directory="files/input",
+        output_directory="files/query_1",
+    )
 
 
 if __name__ == "__main__":
